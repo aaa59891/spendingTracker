da18445bfbc4fdaeaa46793ba2e03fb245e54fb7
Chong <aaa59891@gmail.com>
2018-06-15 22:41:03 +0800

    Updated category item html

--- src/app/components/categories-list/categories-list.component.ts
+++ src/app/components/categories-list/categories-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Category } from "../../models/category";
 
 @Component({
-  selector: 'app-categories-list',
-  templateUrl: './categories-list.component.html',
-  styleUrls: ['./categories-list.component.css']
+    selector: "app-categories-list",
+    templateUrl: "./categories-list.component.html",
+    styleUrls: ["./categories-list.component.css"]
 })
 export class CategoriesListComponent implements OnInit {
+    categories: Category[] = [
+        new Category('test'),
+        new Category('test1'),
+        new Category('test2'),
+        new Category('test3'),
+    ]
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
--- src/app/components/category-item/category-item.component.ts
+++ src/app/components/category-item/category-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Category } from "../../models/category";
 
 @Component({
-  selector: 'app-category-item',
-  templateUrl: './category-item.component.html',
-  styleUrls: ['./category-item.component.css']
+    selector: "app-category-item",
+    templateUrl: "./category-item.component.html",
+    styleUrls: ["./category-item.component.css"]
 })
 export class CategoryItemComponent implements OnInit {
+    @Input('categoryItem') categoryItem: Category;
+    
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        console.log(this.categoryItem);
+    }
 }
